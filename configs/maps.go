e3ac77ac3af4f5dd0470fcda15740a72dc14b8d3
adimasmudi <adimasmudi@gmail.com>
2023-03-12 16:48:12 +0700

    put maps credential in env

--- configs/maps.go
+++ configs/maps.go
@@ -1,9 +1,13 @@
 package configs
 
-import "googlemaps.github.io/maps"
+import (
+	"os"
+
+	"googlemaps.github.io/maps"
+)
 
 func InitMap() (*maps.Client, error){
-	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyB8Xcw0-bTqcs2vXOQ5SANu65-4IR1rRFc"))
+	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("GOOGLE_MAPS_CREDENTIAL")))
 
 	if err != nil{
 		return c, err
